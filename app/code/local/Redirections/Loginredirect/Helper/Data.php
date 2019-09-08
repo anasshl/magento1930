9ac93369aea972ffbe34d8d9673da03426b4d9ee
Anass Hliouat <anasshliouat@gmail.com>
2019-09-08 22:52:51 +0200

    first commit

---
+++ app/code/local/Redirections/Loginredirect/Helper/Data.php
@@ -0,0 +1,28 @@
+<?php
+
+class Redirections_Loginredirect_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    const XML_CONFIG_PATH = 'loginredirect/settings/';
+    public function getConfigValue($key, $store = '')
+    {
+        return Mage::getStoreConfig(self::XML_CONFIG_PATH . $key, $store);
+    }
+    /**
+     * Return path for redirect
+     *
+     * @return string
+     */
+    public function getRedirectUrl()
+    {
+        $_path = (string) $this->_getConfigValue('path_redirect');
+        return Mage::getUrl($_path);
+    }
+    public function isEnabled()
+    {
+        return (bool) $this->_getConfigValue('enabled');
+    }
+    protected function _getConfigValue($key)
+    {
+        return Mage::getStoreConfig(self::XML_CONFIG_PATH . $key);
+    }
+}
---
+++ app/code/local/Redirections/Loginredirect/Model/Observer/Customer.php
@@ -0,0 +1,33 @@
+<?php
+
+class Redirections_Loginredirect_Model_Observer_Customer
+{
+    public function customerLogin(Varien_Event_Observer $observer)
+    {
+        if (Mage::helper('loginredirect')->isEnabled()) {
+            $_session = $this->_getSession();
+            /* Check the user's group and do the redirection.*/
+            if($this->_checkActionForCustomerGroup()) {
+                $_session->setBeforeAuthUrl(Mage::helper('loginredirect')->getRedirectUrl());
+            }
+        }
+    }
+    protected function _checkActionForCustomerGroup()
+    {
+        /* @var $customer Mage_Customer_Model_Customer */
+        $customer = $this->_getSession()->getCustomer();
+        if($customer) {
+            if($customer->getGroupId() == Mage::helper('loginredirect')->getConfigValue('group')) {
+                return TRUE;
+            }
+        }
+        return FALSE;
+    }
+    /**
+     * @return Mage_Core_Model_Abstract
+     */
+    protected function _getSession()
+    {
+        return Mage::getSingleton('customer/session');
+    }
+}
